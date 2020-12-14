ae7865814d389cd4b7fe6dcfa821bd366332a3f4
QuyThanh <quythanh24@gmail.com>
2020-12-14 17:43:25 +0700

    no message

--- app/src/main/java/com/quyT/sodokuresovle/MainActivity.kt
+++ app/src/main/java/com/quyT/sodokuresovle/MainActivity.kt
@@ -1,57 +1,90 @@
 package com.quyT.sodokuresovle
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
 import com.quyT.sodokuresovle.databinding.ActivityMainBinding
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     val listData = ArrayList<ArrayList<Int>>()
     lateinit var binding: ActivityMainBinding
-    var adapter : RootAdapter? = null
+    var adapter: RootAdapter? = null
+
     //
     lateinit var markRow: Array<BooleanArray>
     lateinit var markCol: Array<BooleanArray>
     lateinit var markMatrix: Array<Array<BooleanArray>>
+
     //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        for (i in 0..8) {
-            val listInt = ArrayList<Int>()
-            for (j in 0..8) {
-                listInt.add(0)
+        //
+        Glide.with(this).load(getBoxImage(8, 8)).into(binding.ivTest)
+//        for (i in 0..8) {
+//            val listInt = ArrayList<Int>()
+//            for (j in 0..8) {
+//                listInt.add(0)
+//            }
+//            listData.add(listInt)
+//        }
+//        adapter = RootAdapter(ArrayList(listData))
+//        binding.rvList.adapter = adapter
+//        binding.rvList.layoutManager = LinearLayoutManager(this)
+//
+//
+//        binding.btn.setOnClickListener {
+//            getData()
+//            resolve()
+//        }
+    }
+
+    private fun getTextFromImage(bitmap : Bitmap,succ) {
+        val recognizer = TextRecognition.getClient()
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val result = recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                return visionText.text
             }
-            listData.add(listInt)
-        }
-        adapter = RootAdapter(ArrayList(listData))
-        binding.rvList.adapter = adapter
-        binding.rvList.layoutManager = LinearLayoutManager(this)
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+            }
+    }
 
+    private fun getBoxImage(i: Int, j: Int): Bitmap {
+        val image = BitmapFactory.decodeResource(this.resources, R.drawable.test_iamge_2)
+        val valueNum = image.width / 9
+        val START_X = i * valueNum
+        val START_Y = j * valueNum
+        val WIDTH_PX = valueNum
+        val HEIGHT_PX = valueNum
 
-        binding.btn.setOnClickListener {
-            getData()
-            resolve()
-        }
+        val newBitmap =
+            Bitmap.createBitmap(image, START_X, START_Y, WIDTH_PX, HEIGHT_PX, null, false)
+        return newBitmap
     }
 
     private fun getData() {
         for (i in 0..8) {
             for (j in 0..8) {
-                listData[i][j] = getNum(i,j)
+                listData[i][j] = getNum(i, j)
             }
         }
     }
 
     private fun getNum(i: Int, j: Int): Int {
         val rootViewHolder = binding.rvList.findViewHolderForLayoutPosition(i) as RootViewHolder
-        val viewHolder = rootViewHolder.binding.rvNum.findViewHolderForLayoutPosition(j) as ViewHolder
+        val viewHolder =
+            rootViewHolder.binding.rvNum.findViewHolderForLayoutPosition(j) as ViewHolder
         return if (viewHolder.binding.etNum.text.toString().isNotEmpty())
             viewHolder.binding.etNum.text.toString().toInt()
         else
@@ -103,7 +136,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleResult() {
-        Log.d("Matrix",listData.toString())
+        Log.d("Matrix", listData.toString())
         adapter?.setData(listData)
 
     }
